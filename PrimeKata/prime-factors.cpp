71bf86c5956e40f7eb502ad6ee4f57d2d8d3644b
Hyunseok <lukiaeve@gmail.com>
2025-07-02 10:03:35 +0900

    [refactoring] 50p

--- PrimeKata/prime-factors.cpp
+++ PrimeKata/prime-factors.cpp
@@ -5,14 +5,9 @@ class PrimeFactors {
 public:
     vector<int> of(int number) {
         vector<int> result = {};
-        if (number == 2) {
-            result.push_back(2);
+        if (number > 1) {
+            result.push_back(number);
         }
-
-        if (number == 3) {
-            result.push_back(3);
-        }
-
 
         return result;
     }
