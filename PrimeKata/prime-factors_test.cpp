74f8f4a0a4793ff2713e2c3285534e9f3a07bbbe
Hyunseok <lukiaeve@gmail.com>
2025-07-02 10:05:31 +0900

    [feature] 52p

--- PrimeKata/prime-factors_test.cpp
+++ PrimeKata/prime-factors_test.cpp
@@ -15,13 +15,18 @@ TEST_F(PrimeFixture, Of1) {
 }
 
 TEST_F(PrimeFixture, Of2) {
-    expected = {2};
+    expected = { 2 };
     EXPECT_EQ(expected, prime_factor.of(2));
 }
 
 TEST_F(PrimeFixture, Of3) {
-    expected = {3};
+    expected = { 3 };
     EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST_F(PrimeFixture, Of4) {
+    expected = { 2, 2 };
+    EXPECT_EQ(expected, prime_factor.of(4));
 }
 
 
